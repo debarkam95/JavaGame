fc884e4075a2539c87f598c305626ad9df281aa9
debarkam95 <debarkyo@gmail.com>
2017-12-30 16:20:53 +0530

    user movement achieved

--- Game.java
+++ Game.java
@@ -2,6 +2,8 @@
 import java.io.*;
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
 import java.lang.Math;
 
 class UserBot
@@ -37,6 +39,10 @@ class UserBot
         curLife=curLife+a;
     }
 
+    public int returnCurLife()
+    {
+        return curLife;
+    }
 }
 
 
@@ -180,6 +186,7 @@ class Food
 class Board extends JFrame
 {
     private char pBoard[][]= new char[50][50];
+    int userMoves;
 
     public Board()
     {
@@ -211,13 +218,22 @@ class Board extends JFrame
     public void copyUser(UserBot u)
     {
         pBoard[u.getX()][u.getY()]='u';
+        userMoves=u.returnCurLife();
     }
+
+
+
     public void paint(Graphics g)
     {
         g.setColor(Color.BLACK);
         g.drawLine(0,300,600,300);
         g.drawLine(300,0,300,600);
         g.drawLine(0,600,600,600);
+        g.setColor(Color.WHITE);
+        g.fillRect(0,601,600,100);
+        g.setColor(Color.BLACK);
+        g.drawString("Life : "+Integer.toString(userMoves),50,650);
+
         for(int i=0;i<50;i++)
         for(int j=0;j<50;j++)
         {
@@ -238,6 +254,14 @@ class Board extends JFrame
                 //draw rectangle outline
                 g.fillRect(12*i,12*j,12,12);    
             }
+            else if(pBoard[i][j]=='e')
+            {
+                //set color to Green
+                g.setColor(Color.WHITE);
+                pBoard[i][j]='n';
+                //draw rectangle outline
+                g.fillRect(12*i,12*j,12,12);    
+            }
         }
 
     }
@@ -249,20 +273,71 @@ class Board extends JFrame
 }
 
 
-
-public class Game
+public class Game extends KeyAdapter
 {
- 
-    public static void main(String argv[])throws Exception
+
+    Board b=new Board();
+    Food f=new Food();
+    UserBot ub=new UserBot();
+
+    public Game()
     {
-        Board b=new Board();
-        Food f=new Food();
-        UserBot ub=new UserBot();
-
+        b.setBackground(Color.WHITE);
         b.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-
+        b.addKeyListener(this);
         b.copyFood(f);
         b.copyUser(ub);
         b.renderBoard();
+    }
+
+    public void keyPressed(KeyEvent evt)
+    {
+        int x=ub.getX();
+        int y=ub.getY();
+
+        if(evt.getKeyChar()=='w' && y>0)
+        {
+            ub.updateXY(x, y-1);
+            b.updatePosition(x, y, 'e');
+            b.updatePosition(x, y-1, 'u');
+            ub.updateCurLife(-1);
+            b.renderBoard();
+        }
+        else if(evt.getKeyChar()=='a' && x>0)
+        {
+            ub.updateXY(x-1, y);
+            b.updatePosition(x, y, 'e');
+            b.updatePosition(x-1, y, 'u');
+            ub.updateCurLife(-1);
+            b.renderBoard();
+        }
+        else if(evt.getKeyChar()=='s' && y<49)
+        {
+            ub.updateXY(x, y+1);
+            b.updatePosition(x, y, 'e');
+            b.updatePosition(x, y+1, 'u');
+            ub.updateCurLife(-1);
+            b.renderBoard();
+        }
+        else if(evt.getKeyChar()=='d' && x<49)
+        {
+            ub.updateXY(x+1, y);
+            b.updatePosition(x, y, 'e');
+            b.updatePosition(x+1, y, 'u');
+            ub.updateCurLife(-1);
+            b.renderBoard();
+        }
+        b.copyUser(ub);
+    }
+
+}
+
+class GamePlay
+{
+    public static void main(String argv[])throws Exception
+    {
+        
+        Game g=new Game();
+        
     }
 }
